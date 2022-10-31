0a968920d5d23ab10d5613a6f810804df021e160
Willian Lima <william_slayer@hotmail.com>
2022-10-31 09:00:42 -0300

    Inicindo componente criar pensamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CabecalhoComponent } from './component/cabecalho/cabecalho.component';
+import { CriarPensamentosComponent } from './component/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { RodapeComponent } from './component/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentosComponent
    ],
   imports: [
     BrowserModule,
