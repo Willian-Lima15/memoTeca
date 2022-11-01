6d47a550affc6b6a9b8741f16152a7be860e86b0
Willian Lima <william_slayer@hotmail.com>
2022-11-01 16:31:21 -0300

    Criado backend generico

--- src/app/component/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/component/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -10,14 +10,20 @@ export class ListarPensamentosComponent implements OnInit {
   listaPensamentos = [
     {
       conteudo: 'Primeiro texto',
-          autor: 'Wlises',
-          modelo: 'modelo3'
+      autor: 'Wlises',
+      modelo: 'modelo3'
     },
     {
       conteudo: 'Segundo texto',
-          autor: 'Willian',
-          modelo: 'modelo2'
+      autor: 'Willian',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe eligendi enim distinctio voluptatibus beatae voluptates vitae, impedit, quae libero dignissimos, dolores nam nesciunt similique illo repellat inventore quaerat nihil voluptate?',
+      autor: 'Lorem',
+      modelo: 'modelo1'
     }
+
   ];
 
   constructor() { }
